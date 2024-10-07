6d9a0afb7772af258dbf8a07a4b8a51e5c96b982
olga-taranenko <157386111+olga-taranenko@users.noreply.github.com>
2024-10-07 17:07:22 +0300

    add files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import { setupServer } from './server.js';
 
-setupServer();
+import { initMongoConnection } from './db/initMongoConnection.js';
+
+async function bootstrap() {
+  try {
+    await initMongoConnection();
+
+    setupServer();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+bootstrap();

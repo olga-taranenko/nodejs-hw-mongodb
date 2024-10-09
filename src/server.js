37516a4cd7698e6a849903671293ef31402c117f
olga-taranenko <157386111+olga-taranenko@users.noreply.github.com>
2024-10-09 21:46:49 +0300

    fixed

--- src/server.js
+++ src/server.js
@@ -48,7 +48,7 @@ export const setupServer = () => {
 
       res.status(200).json({
         status: 200,
-        message: 'Successfully found contact with id {contactId}!',
+        message: `Successfully found contact with id ${contactId}!`,
         data: contact,
       });
     } catch (error) {

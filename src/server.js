0eca1fd025fede7aef813033c3530827df64b8cf
olga-taranenko <157386111+olga-taranenko@users.noreply.github.com>
2024-11-13 10:10:36 +0200

    add swagger

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import authRouter from './routers/auth.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -31,6 +32,7 @@ export const setupServer = () => {
   app.use('/contacts', contactsRouter);
 
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.use('*', notFoundHandler);
 
